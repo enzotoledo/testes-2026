b0e4fcd3a9f183424a1021fc9b4105c24cafd55e
enzotoledo <90660486+enzotoledo@users.noreply.github.com>
2026-02-06 21:31:05 -0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		int idade = 41;
+		const nome = "dogras";
+		Console.WriteLine("olá meu nome é" + nome + "e tenho" + idade"anos");
+    }
+}
