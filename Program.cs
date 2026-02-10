c73ab847fe2ba28fea31b81dbe567990f8c0f0f9
enzotoledo <90660486+enzotoledo@users.noreply.github.com>
2026-02-09 21:41:27 -0300

    você é o dougras?

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace whatsYourNameApp
+{
+    class Program
+    {
+        static void Main()
+        {
+           //const string name = "enzo";
+           Console.WriteLine("qual é o seu nome?");
+           string? name = Console.ReadLine();
+            if  (name == "dogras")
+            Console.WriteLine("você é o dogras");
+           else
+           Console.WriteLine("eu sou o dogras você não é o dogras"); 
+        }
+    }
+}
